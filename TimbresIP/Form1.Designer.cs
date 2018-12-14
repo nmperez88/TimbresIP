c22f07282e427290b75a49c95cd76104c8c227c3
nmperez88 <45083298+nmperez88@users.noreply.github.com>
2018-12-14 13:37:13 -0500

    Cambios en la interfaz

--- TimbresIP/Form1.Designer.cs
+++ TimbresIP/Form1.Designer.cs
@@ -31,33 +31,41 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
             this.groupBoxMainControl = new System.Windows.Forms.GroupBox();
             this.groupBoxSchedule = new System.Windows.Forms.GroupBox();
-            this.groupBoxGeneralSound = new System.Windows.Forms.GroupBox();
-            this.menuStripPrincipal = new System.Windows.Forms.MenuStrip();
-            this.inicioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.configuraciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPageSchedule = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.listViewSchedule = new System.Windows.Forms.ListView();
+            this.columnHeaderNumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeaderStartTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeaderSoundTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeaderTone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeaderEnable = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeaderCall = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeaderObservations = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeaderNumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBoxGeneralSound = new System.Windows.Forms.GroupBox();
+            this.textBoxServer = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.labelServer = new System.Windows.Forms.Label();
+            this.labelPort = new System.Windows.Forms.Label();
+            this.buttonSaveServer = new System.Windows.Forms.Button();
+            this.buttonEditServer = new System.Windows.Forms.Button();
+            this.groupBoxMainControl.SuspendLayout();
             this.groupBoxSchedule.SuspendLayout();
-            this.menuStripPrincipal.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPageSchedule.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBoxMainControl
             // 
-            this.groupBoxMainControl.Location = new System.Drawing.Point(12, 28);
+            this.groupBoxMainControl.Controls.Add(this.buttonEditServer);
+            this.groupBoxMainControl.Controls.Add(this.buttonSaveServer);
+            this.groupBoxMainControl.Controls.Add(this.labelPort);
+            this.groupBoxMainControl.Controls.Add(this.labelServer);
+            this.groupBoxMainControl.Controls.Add(this.textBoxPort);
+            this.groupBoxMainControl.Controls.Add(this.textBoxServer);
+            this.groupBoxMainControl.Location = new System.Drawing.Point(12, 8);
             this.groupBoxMainControl.Name = "groupBoxMainControl";
-            this.groupBoxMainControl.Size = new System.Drawing.Size(637, 83);
+            this.groupBoxMainControl.Size = new System.Drawing.Size(637, 61);
             this.groupBoxMainControl.TabIndex = 0;
             this.groupBoxMainControl.TabStop = false;
             this.groupBoxMainControl.Text = "Controles globales";
@@ -65,52 +73,12 @@
             // groupBoxSchedule
             // 
             this.groupBoxSchedule.Controls.Add(this.tabControl1);
-            this.groupBoxSchedule.Location = new System.Drawing.Point(12, 117);
+            this.groupBoxSchedule.Location = new System.Drawing.Point(12, 75);
             this.groupBoxSchedule.Name = "groupBoxSchedule";
-            this.groupBoxSchedule.Size = new System.Drawing.Size(637, 253);
+            this.groupBoxSchedule.Size = new System.Drawing.Size(637, 298);
             this.groupBoxSchedule.TabIndex = 1;
             this.groupBoxSchedule.TabStop = false;
             this.groupBoxSchedule.Text = "Horarios";
-            // 
-            // groupBoxGeneralSound
-            // 
-            this.groupBoxGeneralSound.Location = new System.Drawing.Point(12, 376);
-            this.groupBoxGeneralSound.Name = "groupBoxGeneralSound";
-            this.groupBoxGeneralSound.Size = new System.Drawing.Size(637, 170);
-            this.groupBoxGeneralSound.TabIndex = 2;
-            this.groupBoxGeneralSound.TabStop = false;
-            this.groupBoxGeneralSound.Text = "Sonidos generales";
-            // 
-            // menuStripPrincipal
-            // 
-            this.menuStripPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.inicioToolStripMenuItem});
-            this.menuStripPrincipal.Location = new System.Drawing.Point(0, 0);
-            this.menuStripPrincipal.Name = "menuStripPrincipal";
-            this.menuStripPrincipal.Size = new System.Drawing.Size(661, 24);
-            this.menuStripPrincipal.TabIndex = 3;
-            this.menuStripPrincipal.Text = "menuStrip1";
-            // 
-            // inicioToolStripMenuItem
-            // 
-            this.inicioToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.configuraciónToolStripMenuItem,
-            this.salirToolStripMenuItem});
-            this.inicioToolStripMenuItem.Name = "inicioToolStripMenuItem";
-            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
-            this.inicioToolStripMenuItem.Text = "Inicio";
-            // 
-            // configuraciónToolStripMenuItem
-            // 
-            this.configuraciónToolStripMenuItem.Name = "configuraciónToolStripMenuItem";
-            this.configuraciónToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.configuraciónToolStripMenuItem.Text = "Configuración";
-            // 
-            // salirToolStripMenuItem
-            // 
-            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
-            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.salirToolStripMenuItem.Text = "Salir";
             // 
             // tabControl1
             // 
@@ -119,7 +87,7 @@
             this.tabControl1.Location = new System.Drawing.Point(6, 19);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(625, 228);
+            this.tabControl1.Size = new System.Drawing.Size(625, 273);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPageSchedule
@@ -128,20 +96,10 @@
             this.tabPageSchedule.Location = new System.Drawing.Point(4, 22);
             this.tabPageSchedule.Name = "tabPageSchedule";
             this.tabPageSchedule.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageSchedule.Size = new System.Drawing.Size(617, 202);
+            this.tabPageSchedule.Size = new System.Drawing.Size(617, 247);
             this.tabPageSchedule.TabIndex = 0;
             this.tabPageSchedule.Text = "tabPage1";
             this.tabPageSchedule.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // listViewSchedule
             // 
@@ -156,10 +114,15 @@
             this.listViewSchedule.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listViewSchedule.Location = new System.Drawing.Point(3, 3);
             this.listViewSchedule.Name = "listViewSchedule";
-            this.listViewSchedule.Size = new System.Drawing.Size(611, 196);
+            this.listViewSchedule.Size = new System.Drawing.Size(611, 241);
             this.listViewSchedule.TabIndex = 0;
             this.listViewSchedule.UseCompatibleStateImageBehavior = false;
             this.listViewSchedule.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeaderNumber
+            // 
+            this.columnHeaderNumber.Text = "No.";
+            this.columnHeaderNumber.Width = 32;
             // 
             // columnHeaderStartTime
             // 
@@ -191,10 +154,79 @@
             this.columnHeaderObservations.Text = "Observaciones";
             this.columnHeaderObservations.Width = 166;
             // 
-            // columnHeaderNumber
+            // tabPage2
             // 
-            this.columnHeaderNumber.Text = "No.";
-            this.columnHeaderNumber.Width = 32;
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(617, 202);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxGeneralSound
+            // 
+            this.groupBoxGeneralSound.Location = new System.Drawing.Point(12, 379);
+            this.groupBoxGeneralSound.Name = "groupBoxGeneralSound";
+            this.groupBoxGeneralSound.Size = new System.Drawing.Size(637, 170);
+            this.groupBoxGeneralSound.TabIndex = 2;
+            this.groupBoxGeneralSound.TabStop = false;
+            this.groupBoxGeneralSound.Text = "Sonidos generales";
+            // 
+            // textBoxServer
+            // 
+            this.textBoxServer.Enabled = false;
+            this.textBoxServer.Location = new System.Drawing.Point(72, 23);
+            this.textBoxServer.Name = "textBoxServer";
+            this.textBoxServer.Size = new System.Drawing.Size(128, 20);
+            this.textBoxServer.TabIndex = 0;
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Enabled = false;
+            this.textBoxPort.Location = new System.Drawing.Point(264, 23);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(65, 20);
+            this.textBoxPort.TabIndex = 1;
+            // 
+            // labelServer
+            // 
+            this.labelServer.AutoSize = true;
+            this.labelServer.Location = new System.Drawing.Point(10, 27);
+            this.labelServer.Name = "labelServer";
+            this.labelServer.Size = new System.Drawing.Size(59, 13);
+            this.labelServer.TabIndex = 2;
+            this.labelServer.Text = "ServidorIP:";
+            // 
+            // labelPort
+            // 
+            this.labelPort.AutoSize = true;
+            this.labelPort.Location = new System.Drawing.Point(217, 27);
+            this.labelPort.Name = "labelPort";
+            this.labelPort.Size = new System.Drawing.Size(41, 13);
+            this.labelPort.TabIndex = 3;
+            this.labelPort.Text = "Puerto:";
+            // 
+            // buttonSaveServer
+            // 
+            this.buttonSaveServer.Enabled = false;
+            this.buttonSaveServer.Location = new System.Drawing.Point(437, 23);
+            this.buttonSaveServer.Name = "buttonSaveServer";
+            this.buttonSaveServer.Size = new System.Drawing.Size(75, 23);
+            this.buttonSaveServer.TabIndex = 4;
+            this.buttonSaveServer.Text = "Guardar";
+            this.buttonSaveServer.UseVisualStyleBackColor = true;
+            this.buttonSaveServer.Click += new System.EventHandler(this.buttonSaveServer_Click);
+            // 
+            // buttonEditServer
+            // 
+            this.buttonEditServer.Location = new System.Drawing.Point(347, 23);
+            this.buttonEditServer.Name = "buttonEditServer";
+            this.buttonEditServer.Size = new System.Drawing.Size(75, 23);
+            this.buttonEditServer.TabIndex = 5;
+            this.buttonEditServer.Text = "Editar";
+            this.buttonEditServer.UseVisualStyleBackColor = true;
+            this.buttonEditServer.Click += new System.EventHandler(this.buttonEditServer_Click);
             // 
             // FormPrincipal
             // 
@@ -204,18 +236,15 @@
             this.Controls.Add(this.groupBoxGeneralSound);
             this.Controls.Add(this.groupBoxSchedule);
             this.Controls.Add(this.groupBoxMainControl);
-            this.Controls.Add(this.menuStripPrincipal);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MainMenuStrip = this.menuStripPrincipal;
             this.Name = "FormPrincipal";
             this.Text = "Administrador de Horarios";
+            this.groupBoxMainControl.ResumeLayout(false);
+            this.groupBoxMainControl.PerformLayout();
             this.groupBoxSchedule.ResumeLayout(false);
-            this.menuStripPrincipal.ResumeLayout(false);
-            this.menuStripPrincipal.PerformLayout();
             this.tabControl1.ResumeLayout(false);
             this.tabPageSchedule.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -224,10 +253,6 @@
         private System.Windows.Forms.GroupBox groupBoxMainControl;
         private System.Windows.Forms.GroupBox groupBoxSchedule;
         private System.Windows.Forms.GroupBox groupBoxGeneralSound;
-        private System.Windows.Forms.MenuStrip menuStripPrincipal;
-        private System.Windows.Forms.ToolStripMenuItem inicioToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem configuraciónToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPageSchedule;
         private System.Windows.Forms.ListView listViewSchedule;
@@ -239,6 +264,12 @@
         private System.Windows.Forms.ColumnHeader columnHeaderCall;
         private System.Windows.Forms.ColumnHeader columnHeaderObservations;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button buttonSaveServer;
+        private System.Windows.Forms.Label labelPort;
+        private System.Windows.Forms.Label labelServer;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.TextBox textBoxServer;
+        private System.Windows.Forms.Button buttonEditServer;
     }
 }
 
--- TimbresIP/Form1.cs
+++ TimbresIP/Form1.cs
@@ -16,5 +16,26 @@ namespace TimbresIP
         {
             InitializeComponent();
         }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonEditServer_Click(object sender, EventArgs e)
+        {
+            this.textBoxServer.Enabled = true;
+            this.textBoxPort.Enabled = true;
+            this.buttonSaveServer.Enabled = true;
+            this.buttonEditServer.Enabled = false;
+        }
+
+        private void buttonSaveServer_Click(object sender, EventArgs e)
+        {
+            this.textBoxServer.Enabled = false;
+            this.textBoxPort.Enabled = false;
+            this.buttonSaveServer.Enabled = false;
+            this.buttonEditServer.Enabled = true;
+        }
     }
 }
