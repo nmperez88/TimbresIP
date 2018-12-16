a17d425911829b24f07c9cac6f0ee21a3e8b7d8d
nmperez88 <45083298+nmperez88@users.noreply.github.com>
2018-12-16 17:51:05 -0500

    Interface update

--- TimbresIP/Properties/Resources.Designer.cs
+++ TimbresIP/Properties/Resources.Designer.cs
@@ -131,6 +131,16 @@ namespace TimbresIP.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon iconfinder_sound_115787 {
+            get {
+                object obj = ResourceManager.GetObject("iconfinder_sound_115787", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap information24x24 {
