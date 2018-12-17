2755545d2706c022321419bb7d218607ab7e6dc7
roy <roy@roy.com>
2018-12-17 13:41:00 -0300

    Actualización. Clases

---
+++ TimbresIP/Model/SoundFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimbresIP.Model
+{
+    /**
+     * Archivo de sonido utilizado en los tonos de llamada del horario
+     */
+    class SoundFile
+    {
+        private String name;
+        private String randomId;//nombre de archivo en el directorio destino
+        private String sourcePath;
+        private String targetPath;
+    }
+}
