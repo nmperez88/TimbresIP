f0931aed12e798865989bf4c1091a016e94f6cb4
roy <roy@roy.com>
2018-12-18 13:13:57 -0300

    Actualización

--- TimbresIP/Utils/Json.cs
+++ TimbresIP/Utils/Json.cs
@@ -1,12 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
 
 namespace TimbresIP.Utils
 {
+    /*
+     * Crear y actualizar archivo JSON
+     * 
+     */
     class Json
     {
+        private String path;
+        private String name;
+        private String fullPath;
+        private Object contentObject;
+
+        public Json(string path, string name, object contentObject)
+        {
+            this.path = path;
+            this.name = name;
+            this.fullPath = path + "/" + name;
+            checkExtension();
+            this.contentObject = contentObject;
+        }
+
+        public Json(string fullPath, object contentObject)
+        {
+
+            this.fullPath = fullPath;
+            checkExtension();
+            this.contentObject = contentObject;
+        }
+
+        /*
+         * Crear o actualizar
+         * 
+         */
+        public void serialize()
+        {
+            string outputJSON = JsonConvert.SerializeObject(contentObject);
+            File.WriteAllText(fullPath, outputJSON);
+        }
+
+        /*
+         * Obtener contenido
+         * 
+         */
+        public Object deserialize()
+        {
+            string outputJSON = File.ReadAllText(fullPath);
+            return JsonConvert.DeserializeObject(outputJSON);
+        }
+
+        /*
+         * Chequear tenga extensión, agregarla en caso de no tenerla
+         * 
+         */
+        public void checkExtension()
+        {
+            if (!fullPath.EndsWith(Properties.Settings.Default.jsonExtension))
+            {
+                fullPath += Properties.Settings.Default.jsonExtension;
+            }
+
+        }
+
     }
 }
