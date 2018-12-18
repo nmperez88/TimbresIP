78d4aee6a43f2eef887b4961b4ca518a48b9bcd4
roy <roy@roy.com>
2018-12-18 16:03:32 -0300

    Actualización

---
+++ TimbresIP/Model/AutomaticRingSystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimbresIP.Model
+{
+    /*
+     * Sistema de Timbres Automáticos
+     * 
+     */
+    class AutomaticRingSystem
+    {
+        static Boolean registrationRequired = true;
+        static String userName = "1230";
+        static String displayName = "1230";
+        static String authenticationId = "1230";
+        static String registerPassword = "1230IA";
+        static String domainHost = "100.50.40.3";
+        static Int32 domainPort = 5060;
+        static String numberToDial = "1300";
+        static String audioFile = "helloworld.mp3";
+
+    }
+}
